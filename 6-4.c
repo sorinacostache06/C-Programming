fb5af2f9d52f7561fe81cc9b8a61f030038a8f2d
sacostache <sorinacostache06@yahoo.com>
2017-12-13 11:44:00 +0200

    add 6-4

--- 6-4.c
+++ 6-4.c
@@ -12,27 +12,42 @@ struct tnode {
     struct tnode *right;
 };
 
+struct frequency {
+    struct tnode *word;
+    struct frequency *left;
+    struct frequency *right;
+};
+
+struct frequency *first = NULL;
+
 int getword(char *, int);
 struct tnode *addtree(struct tnode*, char *);
 void printtree(struct tnode*); 
 char *my_strdup(char *);
+struct frequency *create_binay_frequency(struct tnode*, struct frequency*);
+void print_frequency_tree(struct frequency*);
+void create(struct tnode*);
 
 int main()
 {
     char word[MAXWORD];
-    struct tnode *root;
+    struct tnode *root = NULL;
 
     while (getword(word, MAXWORD) != -1) 
         if (isalpha(word[0]))
             root = addtree(root, word);
+
     printtree(root);
+
+    create(root);
+
+    print_frequency_tree(first);
     return 0;
 }
 
 struct tnode *addtree(struct tnode *p, char *w)
 {
     if (p == NULL) {
-        printf("sfsf");
         p = (struct tnode *)malloc(sizeof(struct tnode));
         p->word = my_strdup(w);
         p->count = 1;
@@ -48,12 +63,46 @@ struct tnode *addtree(struct tnode *p, char *w)
     return p;
 }
 
+void create(struct tnode *p)
+{
+    if (p != NULL) {
+        create(p->left);
+        first = create_binay_frequency(p, first);
+        create(p->right);
+        first = create_binay_frequency(p, first);
+    }
+}
+
+struct frequency *create_binay_frequency(struct tnode *p, struct frequency *f)
+{
+    if (f == NULL) {
+        f = (struct frequency *)malloc(sizeof(struct frequency));
+        f->word = p;
+        f->left = f->right = NULL; 
+    } else if (p->count < f->word->count) {
+        f->left = create_binay_frequency(p, f->left);
+    } else if ((p->count > f->word->count) || (p->count == f->word->count)) {
+        f->right = create_binay_frequency(p, f->right);
+    } 
+    
+    return f;
+}
+
 void printtree(struct tnode *p)
 {
     if (p != NULL) {
         printtree(p->left);
         printf("%s %d \n", p->word, p->count);
         printtree(p->right);
+    }
+}
+
+void print_frequency_tree(struct frequency *p)
+{
+    if (p != NULL) {
+        print_frequency_tree(p->right);
+        printf("%s %d \n", p->word->word, p->word->count);
+        print_frequency_tree(p->left);
     }
 }
 
