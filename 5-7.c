e72eef095bc3f7ae66252811ecf1bd83cdb71f13
sacostache <sorinacostache06@yahoo.com>
2018-01-15 16:42:54 +0200

    change with malloc

--- 5-7.c
+++ 5-7.c
@@ -1,45 +1,71 @@
 #include <stdio.h> 
 #include <string.h> 
+#include <time.h>
+#include <stdlib.h>
 
 #define MAXLINES 5000  
 #define MAXLEN 1000
 
 char *lineptr[MAXLINES];
 char  store_array[MAXLINES];
+clock_t start, end;
 
-int readlines(char *lineptr[], char *store_array, int nlines); 
+int readlines(char *lineptr[], int nlines); 
 void writelines(char *lineptr[], int nlines); 
-void qsort(char *lineptr[], int left, int right);      
+void my_qsort(char *lineptr[], int left, int right);      
 int  my_getline(char *, int); 
+char *alloc(int); 
+
+/* Program with malloc is much faster:
+ ana
+are
+mere
+0
+ana
+are
+mere
+Program with malloc took 0.000195 seconds to execute
+
+ana
+are
+mere
+0
+ana
+are
+mere
+Program with alloc took 0.000255 seconds to execute*/
 
 int main() 
 { 
+    double time_malloc;
+    start = clock();
+
     int nlines;    
-    if ((nlines = readlines(lineptr, store_array, MAXLINES)) >= 0) { 
-        qsort(lineptr, 0, nlines-1); 
+    if ((nlines = readlines(lineptr, MAXLINES)) >= 0) { 
+        my_qsort(lineptr, 0, nlines-1); 
         writelines(lineptr, nlines); 
-        return 0; 
+        // return 0; 
     } else { 
         printf("error: input too big to sort\n"); 
-        return 1; 
+        // return 1; 
     } 
+    end = clock();
+    time_malloc = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Program with alloc took %f seconds to execute \n", time_malloc);
 } 
 
-int readlines(char *lineptr[], char *store_array, int maxlines) 
+int readlines(char *lineptr[], int maxlines) 
 { 
     int len, nlines; 
-    char *p = store_array, line[MAXLEN]; 
+    char *p, line[MAXLEN]; 
     nlines = 0; 
-    char *maxline = store_array + MAXLEN;
-
     while ((len = my_getline(line, MAXLEN)) > 0) 
-        if (nlines >= maxlines || p + len > maxline) 
+        if ((nlines >= maxlines) || (p = (char *)malloc(len)) == NULL) 
             return -1; 
         else { 
-            line[len-1] = '\0'; 
+            line[len-1] = '\0';  
             strcpy(p, line); 
             lineptr[nlines++] = p; 
-            p = p + len;
         } 
     return nlines; 
 } 
@@ -51,7 +77,7 @@ void writelines(char *lineptr[], int nlines)
         printf("%s\n", lineptr[i]); 
 }
 
-void qsort(char *v[], int left, int right) 
+void my_qsort(char *v[], int left, int right) 
 { 
     int i, last; 
     void swap(char *v[], int i, int j); 
@@ -63,8 +89,8 @@ void qsort(char *v[], int left, int right)
         if (strcmp(v[i], v[left]) < 0) 
             swap(v, ++last, i); 
     swap(v, left, last); 
-    qsort(v, left, last-1); 
-    qsort(v, last+1, right); 
+    my_qsort(v, left, last-1); 
+    my_qsort(v, last+1, right); 
 } 
  
    void swap(char *v[],int i,int j)
@@ -90,3 +116,20 @@ int my_getline(char *s,int lim)
 
     return s-t;
 }
+
+   #define ALLOCSIZE 10000 /* size of available space */ 
+   static char allocbuf[ALLOCSIZE]; /* storage for alloc */ 
+   static char *allocp = allocbuf;  /* next free position */ 
+   char *alloc(int n)    /* return pointer to n characters */ 
+   { 
+       if (allocbuf + ALLOCSIZE - allocp >= n) {  /* it fits */ 
+           allocp += n; 
+           return allocp - n; /* old p */ 
+       } else      /* not enough room */ 
+           return 0; 
+   } 
+   void afree(char *p)  /* free storage pointed to by p */ 
+   { 
+       if (p >= allocbuf && p < allocbuf + ALLOCSIZE) 
+           allocp = p; 
+   } 
